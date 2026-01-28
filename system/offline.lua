2aa75b28de628e15c872b54c9b1c28477fd27fd7
loldo-g <loldog001@gmail.com>
2026-01-27 21:55:25 -0300

    offline.lua

---
+++ system/offline.lua
@@ -0,0 +1,3 @@
+warn("script está fora do ar no momento")
+warn("talvez está tendo updates ou foi pedido para deletar ele")
+warn("se tiver alguma duvida me chama DM no discord, meu discord é loldo_gg")
